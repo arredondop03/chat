3cd0bb96af5754274a79b22f9021e192ae02f8bd
Paola Arredondo <arredondop03@gmail.com>
2020-08-19 12:34:17 -0400

    fixed extensions linting errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App.jsx';
+import App from './App';
 import 'normalize.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
